2aca276aa27f8d79d0ea79e2bd89fa9d0434787e
Ariana Parquette <ariana.parquette@emory.edu>
2024-04-03 18:44:16 -0400

    milk categorization

---
+++ milk_categories.R
@@ -0,0 +1,13 @@
+#create milk type dataset
+milk <- bananaindex %>%
+  filter(entity %in% c("Almond milk", 
+                       "Soy milk", 
+                       "Oat milk", 
+                       "Coconut milk", 
+                       "Cow milk")) %>% 
+  mutate(milk_type = case_when(
+    entity == "Almond milk" ~ "plant",
+    entity == "Soy milk" ~ "plant",
+    entity == "Oat milk" ~ "plant",
+    entity == "Coconut milk" ~ "plant",
+    entity == "Cow milk" ~ "cow"))
